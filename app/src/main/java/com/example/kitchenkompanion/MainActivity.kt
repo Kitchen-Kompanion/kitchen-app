cd14a9b1c6a84d97a93675de47002e3b078a866c
Kyumin Hwang <khwang77@@umd.edu>
2025-03-01 00:31:21 -0500

    added bar

--- app/src/main/java/com/example/kitchenkompanion/MainActivity.kt
+++ app/src/main/java/com/example/kitchenkompanion/MainActivity.kt
@@ -5,16 +5,45 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    // Source That I used
+    // https://www.youtube.com/watch?v=obYxPd2ot7Q&ab_channel=CodeWithCal
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setUpTabBar()
+    }
+
+    private fun setUpTabBar()
+    {
+        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
+        val viewPager = findViewById<ViewPager2>(R.id.viewPager)
+        val adapter = TabPageAdapter(this, tabLayout.tabCount)
+        viewPager.adapter = adapter
+
+        viewPager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback()
+        {
+            override fun onPageSelected(position: Int) {
+                tabLayout.selectTab(tabLayout.getTabAt(position))
+            }
+        })
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener
+        {
+            override fun onTabSelected(tab: TabLayout.Tab)
+            {
+                viewPager.currentItem = tab.position
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
     }
 }
---
+++ app/src/main/java/com/example/kitchenkompanion/TabPageAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.kitchenkompanion
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class TabPageAdapter(activity: FragmentActivity, private val tabCount: Int) : FragmentStateAdapter(activity)
+{
+    override fun getItemCount(): Int = tabCount
+
+    override fun createFragment(position: Int): Fragment
+    {
+        return when (position)
+        {
+            0 -> TextFragment()
+            1 -> LRFragment()
+            2 -> ColorsFragment()
+            3 -> ProfileFragment()
+            4 ->ChoicesFragment()
+            5 -> ToDoFragment()
+            else -> TextFragment()
+        }
+    }
+
+}
