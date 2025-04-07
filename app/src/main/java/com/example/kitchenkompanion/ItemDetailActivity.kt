8b8537f079d622d1b8ab056fd61c06e924f2de56
Kyumin Hwang <khwang77@@umd.edu>
2025-04-07 01:03:22 -0400

    inventory

--- app/src/main/java/com/example/kitchenkompanion/ItemDetailActivity.kt
+++ app/src/main/java/com/example/kitchenkompanion/ItemDetailActivity.kt
@@ -14,9 +14,15 @@ class ItemDetailActivity : AppCompatActivity() {
 
         item = intent.getSerializableExtra("item") as? InventoryItem ?: return
 
+        val typeSpinner = findViewById<Spinner>(R.id.edit_type)
+        val types = arrayOf("Fruits", "Dairy", "Pantry", "Frozen", "Beverages", "Snacks")
+        typeSpinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, types)
+        val currentIndex = types.indexOf(item.type)
+        if (currentIndex >= 0) {
+            typeSpinner.setSelection(currentIndex)
+        }
 
         val nameEdit = findViewById<EditText>(R.id.edit_name)
-        val typeEdit = findViewById<EditText>(R.id.edit_type)
         val ownerEdit = findViewById<EditText>(R.id.edit_owner)
         val countEdit = findViewById<EditText>(R.id.edit_count)
         val expireEdit = findViewById<EditText>(R.id.edit_expire)
@@ -24,7 +30,7 @@ class ItemDetailActivity : AppCompatActivity() {
 
         // 기존 값 설정
         nameEdit.setText(item.name)
-        typeEdit.setText(item.type)
+
         ownerEdit.setText(item.owner)
         countEdit.setText(item.count.toString())
         expireEdit.setText(item.expireDate)
@@ -36,7 +42,7 @@ class ItemDetailActivity : AppCompatActivity() {
         saveButton.setOnClickListener {
             val updatedItem = InventoryItem(
                 name = nameEdit.text.toString(),
-                type = typeEdit.text.toString(),
+                type =typeSpinner.selectedItem.toString(),
                 owner = ownerEdit.text.toString(),
                 count = countEdit.text.toString().toIntOrNull() ?: 1,
                 expireDate = expireEdit.text.toString()
